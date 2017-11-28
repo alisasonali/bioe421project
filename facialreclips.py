3aa935d61c5be5d706ea970d624598cd69c4d1ef
Team 06 Alisa and Sonali <akm7@rice.edu>
2017-11-27 20:18:29 -0600

    We added the ability to add filters to specific parts of the face and also finalized our project.

---
+++ facialreclips.py
@@ -0,0 +1,142 @@
+#! /usr/bin/python
+
+import cv2  # OpenCV Library
+import os
+ 
+#-----------------------------------------------------------------------------
+#       Load and configure Haar Cascade Classifiers
+#-----------------------------------------------------------------------------
+ 
+# location of OpenCV Haar Cascade Classifiers:
+baseCascadePath = "/home/pi/bioe421project/haarcascades/"
+ 
+# xml files describing our haar cascade classifiers
+faceCascadeFilePath = baseCascadePath + "haarcascade_frontalface_alt.xml"
+noseCascadeFilePath = baseCascadePath + "haarcascade_mcs_mouth.xml"
+ 
+# build our cv2 Cascade Classifiers
+faceCascade = cv2.CascadeClassifier(faceCascadeFilePath)
+noseCascade = cv2.CascadeClassifier(noseCascadeFilePath)
+ 
+#-----------------------------------------------------------------------------
+#       Load and configure mustache (.png with alpha transparency)
+#-----------------------------------------------------------------------------
+ 
+# Load our overlay image: mustache.png
+imgMustache = cv2.imread('/home/pi/bioe421project/drawpicture/Filter.png',-1)
+ 
+# Create the mask for the mustache
+orig_mask = imgMustache[:,:,3]
+ 
+# Create the inverted mask for the mustache
+orig_mask_inv = cv2.bitwise_not(orig_mask)
+ 
+# Convert mustache image to BGR
+# and save the original image size (used later when re-sizing the image)
+imgMustache = imgMustache[:,:,0:3]
+origMustacheHeight, origMustacheWidth = imgMustache.shape[:2]
+ 
+#-----------------------------------------------------------------------------
+#       Main program loop
+#-----------------------------------------------------------------------------
+ 
+# collect video input from first webcam on system
+video_capture = cv2.VideoCapture('find_our_faces.mp4')
+iter = 0
+ 
+while True:
+    iter = iter + 1
+    # Capture video feed
+    #video_capture = cv2.VideoCapture(video_capture) 
+    ret, frame = video_capture.read()
+    # print imgMustache.shape
+    # Create greyscale image from the video feed
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+ 
+    # Detect faces in input video stream
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+    )
+ 
+   # Iterate over each face found
+    for (x, y, w, h) in faces:
+        # Un-comment the next line for debug (draw box around all faces)
+        # face = cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+ 
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+ 
+        # Detect a nose within the region bounded by each face (the ROI)
+        nose = noseCascade.detectMultiScale(roi_gray)
+  
+        for (nx,ny,nw,nh) in nose:
+            # Un-comment the next line for debug (draw box around the nose)
+            #cv2.rectangle(roi_color,(nx,ny),(nx+nw,ny+nh),(255,0,0),2)
+ 
+            # The mustache should be three times the width of the nose
+            mustacheWidth =  3 * nw
+            mustacheHeight = mustacheWidth * origMustacheHeight / origMustacheWidth
+ 
+            # Center the mustache on the bottom of the nose
+            x1 = nx #- (mustacheWidth/4)
+            x2 = nx + nw #+ (mustacheWidth/4)
+            y1 = ny + nh - (mustacheHeight/2)
+            y2 = ny + nh +(mustacheHeight/2)
+ 
+            # Check for clipping
+            if x1 < 0:
+                x1 = 0
+            if y1 < 0:
+                y1 = 0
+            if x2 > w:
+                x2 = w
+            if y2 > h:
+                y2 = h
+ 
+            # Re-calculate the width and height of the mustache image
+            mustacheWidth = x2 - x1
+            mustacheHeight = y2 - y1
+
+            # Re-size the original image and the masks to the mustache sizes
+            # calcualted above
+            mustache = cv2.resize(imgMustache, (mustacheWidth,mustacheHeight), interpolation = cv2.INTER_AREA)
+            mask = cv2.resize(orig_mask, (mustacheWidth,mustacheHeight), interpolation = cv2.INTER_AREA)
+            mask_inv = cv2.resize(orig_mask_inv, (mustacheWidth,mustacheHeight), interpolation = cv2.INTER_AREA)
+ 
+            # take ROI for mustache from background equal to size of mustache image
+            roi = roi_color[y1:y2, x1:x2]
+ 
+            # roi_bg contains the original image only where the mustache is not
+            # in the region that is the size of the mustache.
+            roi_bg = cv2.bitwise_and(roi,roi,mask = mask_inv)
+ 
+            # roi_fg contains the image of the mustache only where the mustache is
+            roi_fg = cv2.bitwise_and(mustache,mustache,mask = mask)
+ 
+            # join the roi_bg and roi_fg
+            dst = cv2.add(roi_bg,roi_fg)
+ 
+            # place the joined image, saved to dst back over the original image
+            roi_color[y1:y2, x1:x2] = dst
+ 
+            break
+ 
+    # Display the resulting frame
+    cv2.imshow('hii Video', frame)
+    cv2.imwrite("/home/pi/bioe421project/modifiedframes/{0:d}.png".format(iter), frame)
+ 
+    # press any key to exit
+    # NOTE;  x86 systems may need to remove: " 0xFF == ord('q')"
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+os.system("ffmpeg -f image2 -framerate 12 -i /home/pi/bioe421project/modifiedframes/%d.png playback.avi")
+os.system("omxplayer playback.avi")
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
--- guitest.py
+++ guitest.py
@@ -12,8 +12,14 @@ myFont = tkFont.Font(family = 'Impact', size = 30)
 def VideoON():
 	os.system("python /home/pi/bioe421project/SaveVideo.py")
 
-def FilterON():
+def FilterMustacheON():
         os.system("python /home/pi/bioe421project/facialrec.py")
+
+def FilterEyesON():
+        os.system("python /home/pi/bioe421project/facialreceyes.py")
+
+def FilterLipsON():
+        os.system("python /home/pi/bioe421project/facialreclips.py")
 
 def DrawON():
 	os.system("processing-java --sketch=/home/pi/bioe421project/drawpicture --run")
@@ -31,15 +37,21 @@ win.configure(bg = 'sky blue')
 exitButton  = Button(win, text = "Exit", font = myFont, command = exitProgram, height = 1 , width = 4, background = 'red', foreground = 'white') 
 exitButton.pack(side = BOTTOM)
 
-VideoButton = Button(win, text = "Take a video!", font = myFont, command = VideoON, height = 2, width = 14, background = 'deep pink', foreground = 'ivory' )
+VideoButton = Button(win, text = "Take a video!", font = myFont, command = VideoON, height = 1, width = 14, background = 'deep pink', foreground = 'ivory' )
 VideoButton.pack()
 
-DrawButton = Button(win, text = "Draw a filter!", font = myFont, command = DrawON, height = 2, width = 14, background = 'coral', foreground = 'ivory' )
+DrawButton = Button(win, text = "Draw a filter!", font = myFont, command = DrawON, height = 1, width = 14, background = 'coral', foreground = 'ivory' )
 DrawButton.pack()
 
 
-FilterButton = Button(win, text = "Add your filter!", font = myFont, command = FilterON, height = 2, width = 14 , background = 'teal', foreground = 'ivory' )
-FilterButton.pack()
+FilterMustacheButton = Button(win, text = "Add your filter below the nose!", font = myFont, command = FilterMustacheON, height = 1, width = 24 , background = 'teal', foreground = 'ivory' )
+FilterMustacheButton.pack()
+
+FilterEyesButton = Button(win, text = "Add your filter on the eyes!", font = myFont, command = FilterEyesON, height = 1, width = 24 , background = 'teal', foreground = 'ivory' )
+FilterEyesButton.pack()
+
+FilterLipsButton = Button(win, text = "Add your filter on the lips!", font = myFont, command = FilterLipsON, height = 1, width = 24 , background = 'teal', foreground = 'ivory' )
+FilterLipsButton.pack()
 
 mainloop()
 
